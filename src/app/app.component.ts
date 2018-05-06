b432c68f44ad4dd997a417a1dd05e2cb33b8e962
Brenda Franco <brenda.leigh.franco@gmail.com>
2018-05-06 16:02:02 -0700

    Add master list of available filters.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Teapot } from './models/teapot.model';
+import { Filter } from './models/filter.model';
 
 @Component({
   selector: 'app-root',
@@ -51,6 +52,24 @@ export class AppComponent {
         '45.00',
         800251,
         [1,2,3,4,5]
+    )
+  ]
+
+  masterFilterList: Filter[] =  [
+    new Filter('ACCESSORIES',
+        ['with infuser', 'with lid']
+    ),
+    new Filter('CAPACITY',
+        ['up to 15 oz', '15 to 25 oz', '25 to 35 oz']
+    ),
+    new Filter('CARE',
+        ['dishwasher safe', 'microwave safe']
+    ),
+    new Filter('SPECIAL MATERIALS',
+        ['bone china']
+    ),
+    new Filter('STYLE',
+        ['asian', 'traditional', 'everyday', 'whimsical', 'glass']
     )
   ]
 }
