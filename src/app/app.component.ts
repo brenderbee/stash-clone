61241faae556ffcac9ac521de3419a7a68d4f223
Brenda Franco <brenda.leigh.franco@gmail.com>
2018-05-08 13:00:38 -0700

    Remove masterFilterList from app.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,33 +8,5 @@ import { Filter } from './models/filter.model';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  // masterFilterList: Filter[] =  [
-  //   new Filter('ACCESSORIES',
-  //       ['with infuser', 'with lid']
-  //   ),
-  //   new Filter('CAPACITY',
-  //       ['up to 15 oz', '15 to 25 oz', '25 to 35 oz']
-  //   ),
-  //   new Filter('CARE',
-  //       ['dishwasher safe', 'microwave safe']
-  //   ),
-  //   new Filter('SPECIAL MATERIALS',
-  //       ['bone china']
-  //   ),
-  //   new Filter('STYLE',
-  //       ['asian', 'traditional', 'everyday', 'whimsical', 'glass']
-  //   )
-  // ]
-  // sort: string = "allTeapots";
-  //
-  // onChange(optionFromMenu) {
-  //   this.sort = optionFromMenu;
-  // }
-  //
-  // filterParam = null;
-  //
-  // filterParamSet(param) {
-  //   this.filterParam = param;
-  //   console.log("this is at the parent: " + this.filterParam);
-  // }
+
 }
