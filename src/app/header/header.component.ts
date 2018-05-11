275e02b101b91803ce594a8d64b9d28b3e7eae7b
Brenda Franco <brenda.leigh.franco@gmail.com>
2018-05-11 12:51:58 -0700

    Remove testNumbers and show shopping cart items in basket button.

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -6,19 +6,17 @@ import { ShoppingCartService } from './../shopping-cart.service';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  // styleUrls: ['./header.component.css']
   styleUrls: ['./header.component.css'],
   providers: [ShoppingCartService]
 })
 export class HeaderComponent implements OnInit {
 
   items: Teapot[] = [];
-  numbers: Array<number> = [];
+
   constructor(public shoppingCartService: ShoppingCartService) { }
 
   ngOnInit() {
     this.items = this.shoppingCartService.getItems();
-    this.numbers = this.shoppingCartService.getTestNumbers();
   }
 
 }
--- src/app/shopping-cart.service.ts
+++ src/app/shopping-cart.service.ts
@@ -7,18 +7,12 @@ let items: Teapot[] = []; //I actually came up with this part.
 export class ShoppingCartService {
 
   items = items; //I threw out a lifeline for this. Asked husband for help and he identified a syntax error.
-  numbers: Array<number> = [1,2,3,4];
 
   getItems() {
     return this.items;
   }
 
-  getTestNumbers() {
-    return this.numbers;
-  }
-
   pushItem(clickedTeapot: Teapot){
-    console.log("this is at the service: " , this.items);
     return this.items.push(clickedTeapot);
   }
 
