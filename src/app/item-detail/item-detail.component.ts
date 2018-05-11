a30b64b3f32c2ca4280729adcebc9d29c52a84bc
Brenda Franco <brenda.leigh.franco@gmail.com>
2018-05-11 16:21:59 -0700

    Add shopping cart service to detail view.

--- src/app/item-detail/item-detail.component.ts
+++ src/app/item-detail/item-detail.component.ts
@@ -4,21 +4,24 @@ import { Location } from '@angular/common';
 import { Teapot } from '../models/teapot.model';
 import { TeapotService } from '../teapot.service';
 import { FirebaseObjectObservable } from 'angularfire2/database';
+import { ShoppingCartService } from './../shopping-cart.service';
 
 @Component({
   selector: 'app-item-detail',
   templateUrl: './item-detail.component.html',
   styleUrls: ['./item-detail.component.css'],
-  providers: [TeapotService]
+  providers: [TeapotService, ShoppingCartService]
 })
 export class ItemDetailComponent implements OnInit {
   teapotId: string;
   teapotToDisplay;
+  items: Teapot[] = [];
 
   constructor(
     private route: ActivatedRoute,
     private location: Location,
-    private teapotService: TeapotService) { }
+    private teapotService: TeapotService,
+    public shoppingCartService: ShoppingCartService) { }
 
     ngOnInit() {
       this.route.params.forEach((urlParameters) => {
@@ -26,6 +29,10 @@ export class ItemDetailComponent implements OnInit {
       });
       this.teapotToDisplay = this.teapotService.getTeapotById(this.teapotId);
       console.log("this is the teapot at the detail page on init: " + this.teapotToDisplay);
+    }
+
+    addToCart(clickedTeapot: Teapot) {
+      this.shoppingCartService.pushItem(clickedTeapot);
     }
 
 }
