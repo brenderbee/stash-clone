dd0913cb27672fa83883dac4cbf52d2eeca8ab96
Brenda Franco <brenda.leigh.franco@gmail.com>
2018-05-26 22:02:54 -0700

    Add  to constructor.

--- src/app/models/teapot.model.ts
+++ src/app/models/teapot.model.ts
@@ -3,6 +3,7 @@ export class Teapot {
     public name: string,
     public description: string,
     public colors: Array<string>,
+    public $key: string,
     public infuser: boolean,
     public lid: boolean,
     public capacity: number,
